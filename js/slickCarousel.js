3da0e0de461812d3a92ee50188380ec79267f618
JoshR95 <joshuarickards@hotmail.co.uk>
2024-10-31 15:44:39 +0000

    adding slick()
    i have added slick() and restructured the banner to accommodate it, im now just trying to get it to link correctly without a cdn

---
+++ js/slickCarousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.banner-slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: false
+    });
+});
