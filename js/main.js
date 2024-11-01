cde0f798fe8eb89d1a2a4eafc5318125049291fa
JoshR95 <joshuarickards@hotmail.co.uk>
2024-11-01 11:22:26 +0000

    made the base of the consent pop up and fixed the footer social links at lowest viewport
    i have made the bare bones of the consent pop up. I have also made the footer socials links be in a box formtation at very low viewports

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+////////////////////////////////////////////////////////////////////////////////
+/// SLICK CAROUSEL SETTINGS
+////////////////////////////////////////////////////////////////////////////////
+
 $(document).ready(function(){
     $('.banner-slider').slick({
         dots: true,
@@ -9,5 +13,21 @@ $(document).ready(function(){
         autoplay: false,
         autoplaySpeed: 5000,
         pauseOnHover: false
+    });
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////
+/// MANAGE CONSENT JS 
+//////////////////////////////////////////////////////////////////////////////////
+
+// Wait for the entire document to be fully loaded and ready
+$(document).ready(function() {
+    // Show popup when "Manage consent" is clicked
+    // Select any button inside elements with class 'sticky-consent' and attach a click event handler to those buttons
+    $('.sticky-consent button').click(function() {
+        // When clicked, find elements with class 'cookie-popup' and add the class 'active' to them
+        // when active we give css to display the box
+        $('.cookie-popup').addClass('active');
     });
 });
